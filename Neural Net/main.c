150785a23719e160b4054abb4826317623f65a18
Craig Mellor <neskechastro@gmail.com>
2022-05-01 17:35:59 -0400

    Initial Commit

---
+++ Neural Net/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Neural Net
+//
+//  Created by Shaunte Mellor on 5/1/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
